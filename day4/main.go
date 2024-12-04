4759fb5565dfa1a37ed72b6771adcc0585ea0ab6
gramsco <antoningauth@gmail.com>
2024-12-04 09:06:37 +0100

    setup day4

---
+++ day4/main.go
@@ -0,0 +1,8 @@
+package main
+
+func solvePart1(s string) int {
+	return 0
+}
+
+func main() {
+}
---
+++ day4/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestXxx(t *testing.T) {
+	puzzle := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+	result := solvePart1(puzzle)
+	if result != 18 {
+		t.Fatalf("expected 18, received %d", result)
+	}
+}
